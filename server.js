0464b6accb7678668fea5bb36cf9d6032fd0ba57
xavifortes <itsfortes@gmail.com>
2022-08-09 13:37:13 +0200

    Changed port

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/investments.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8280;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
