167d8c58e6c5e3ddf6aa7f3635831f0787f3cb20
xavifortes <itsfortes@gmail.com>
2022-08-10 17:41:05 +0200

    More

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -17,7 +17,7 @@ exports.getMoney = (req, res) => {
 };
 
 exports.kick = (req, res) => {
-  User.findById(req.id).exec((err, user) => {
+  User.findOne({ discordId: req.body.id }).exec((err, user) => {
     if (err) {
       res.status(500).send({ message: err });
       return;
